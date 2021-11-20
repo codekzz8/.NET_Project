661cf1f9cd2636205c5edbf5c8336af375710ca5
unknown <marius.iurea@yahoo.com>
2021-11-20 13:35:35 +0200

    Built ML model using ML.Net and created bare bones Angular UI for proof of concept.

---
+++ HousePredictionApi/Controllers/PredictController.cs
@@ -0,0 +1,23 @@
+﻿using HousePredictionApiML.Model;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HousePredictionApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PredictController : ControllerBase
+    {
+        [HttpPost]
+        public ModelOutput GetPrediction([FromBody]ModelInput request)
+        {
+            return ConsumeModel.Predict(request);
+        }
+    }
+}
